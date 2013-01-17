c347b5de93084e8e4fbffa2898fc1cfebae9a2f7
Gergely J Szöllősi <sszolo@gmail.com>
2013-01-17 10:32:48 +0100

    First complie and misc. spelling..

--- ALEobserve.cpp
+++ ALEobserve.cpp
@@ -1,4 +1,4 @@
-#include "exODT.h"
+#include "ALE.h"
 #include "ALE_util.h"
 
 using namespace std;
@@ -6,14 +6,14 @@ using namespace bpp;
 int main(int argc, char ** argv)
 {
   string ale_file=argv[1];
-  string ale_name=ale_file+".ale"
+  string ale_name=ale_file+".ale";
   approx_posterior * ale;
   int burnin=0;
   if (argc>2) 
-    burnin=atoi(arg[2]);
+    burnin=atoi(argv[2]);
   ale=obsorve_ALE_from_file(ale_file,burnin);
-  cout << "# obsorved."<<endl;
+  cout << "# observe "<< ale->observations << "trees from: " <<  argv[1] << endl;
   ale->save_state(ale_name);
-  cout << "# saved."<<endl;
-  
+  cout << "# saved in"<< ale_name<<endl;
+  return 1;
 }
