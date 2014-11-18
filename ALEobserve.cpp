37145fd0614a99d473da7f71337b9e61aa56ffe4
ssolo <ssolo@tyan1.elte.hu>
2014-11-18 14:11:23 +0100

    newer smarter

--- ALEobserve.cpp
+++ ALEobserve.cpp
@@ -13,14 +13,26 @@ int main(int argc, char ** argv)
       return 1;
     }
 
-  string ale_file=argv[1];
-  string ale_name=ale_file+".ale";
+  string first_file=argv[1];
+  vector <string> tokens;
+  boost::trim(first_file);	    
+  boost::split(tokens,first_file,boost::is_any_of("."),boost::token_compress_on);
+  string head=tokens[0];
+  
+  vector<string> ale_files;
+  ale_files.push_back(first_file);
+  for (int i=2;i<argc-1;i++)
+    ale_files.push_back(argv[i]);
+  string ale_name=head+".ale";
   approx_posterior * ale;
-  int burnin=0;
+  int burnin=500;
   if (argc>2) 
-    burnin=atoi(argv[2]);
-  ale=observe_ALE_from_file(ale_file,burnin);
-  cout << "# observe "<< ale->observations << "trees from: " <<  argv[1] << endl;
+    burnin=atoi(argv[argc-1]);
+  ale=observe_ALE_from_file(ale_files,burnin);
+  cout << "# observe "<< ale->observations << "trees from: " <<  argv[1] ;
+  for (int i=2;i<argc-1;i++)
+    cout << " " << argv[i];
+  cout << endl;
   ale->save_state(ale_name);
   cout << "# saved in "<< ale_name<<endl;
   return 1;
