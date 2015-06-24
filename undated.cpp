dd04d65a20240d4716ac89ceb8b312459a31b897
Gergely J Szöllősi <sszolo@gmail.com>
2015-06-24 13:09:40 +0200

    fixed Ttokens

--- undated.cpp
+++ undated.cpp
@@ -662,15 +662,7 @@ string exODT_model::sample_undated(int e, int i,string last_event,string branch_
 		  register_T_to_from(e,f);
 
 		  stringstream Ttoken;
-		  if (e<last_leaf)
-		    Ttoken<< extant_species[e];
-		  else
-		    Ttoken<< e;
-		  if (f<last_leaf)
-		    Ttoken<<"|"<<extant_species[f];
-		  else
-		    Ttoken<<"|"<<f;
-		  Ttoken<<"|"<<ale_pointer->set2name(ale_pointer->id_sets[gpp_i]);
+		  Ttoken<<estr<<">"<<fstr<<"|"<<ale_pointer->set2name(ale_pointer->id_sets[gpp_i]);
 		  Ttokens.push_back(Ttoken.str());
 
 		  
@@ -692,7 +684,7 @@ string exODT_model::sample_undated(int e, int i,string last_event,string branch_
 		    Ttoken<<"|"<<extant_species[f];
 		  else
 		    Ttoken<<"|"<<f;
-		  Ttoken<<"|"<<ale_pointer->set2name(ale_pointer->id_sets[gp_i]);
+		  Ttoken<<fstr<<">"<<estr<<"|"<<ale_pointer->set2name(ale_pointer->id_sets[gp_i]);
 		  Ttokens.push_back(Ttoken.str());
 
 		  return "("+sample_undated(e,gpp_i,"S")+","+sample_undated(f,gp_i,"T")+").T@"+estr+"->"+fstr+branch_string+":"+branch_length;
@@ -742,15 +734,7 @@ string exODT_model::sample_undated(int e, int i,string last_event,string branch_
 	  register_T_to_from(e,f);
 	  
 	  stringstream Ttoken;
-	  if (e<last_leaf)
-	    Ttoken<< extant_species[e];
-	  else
-	    Ttoken<< e;
-	  if (f<last_leaf)
-	    Ttoken<<"|"<<extant_species[f];
-	  else
-	    Ttoken<<"|"<<f;
-	  Ttoken<<"|"<<ale_pointer->set2name(ale_pointer->id_sets[g_id]);
+	  Ttoken<<estr<<">"<<fstr"|"<<ale_pointer->set2name(ale_pointer->id_sets[g_id]);
 	  Ttokens.push_back(Ttoken.str());
 	  
 	  register_L(e); 
