75c59eae3a24334b57ec14f78860eb0ca3fc99d0
Gergely J Szöllősi <sszolo@gmail.com>
2015-11-25 16:37:09 +0100

    ..

--- ALEobserve.cpp
+++ ALEobserve.cpp
@@ -14,25 +14,29 @@ int main(int argc, char ** argv)
     }
 
   string first_file=argv[1];
-  vector <string> tokens;
   boost::trim(first_file);	    
-  boost::split(tokens,first_file,boost::is_any_of("."),boost::token_compress_on);
-  string head=tokens[0];
-  
-  vector<string> ale_files;
-  ale_files.push_back(first_file);
-  for (int i=2;i<argc-1;i++)
-    ale_files.push_back(argv[i]);
+  string head=first_file;
   string ale_name=head+".ale";
   approx_posterior * ale;
-  int burnin=500;
-  if (argc>2) 
-    burnin=atoi(argv[argc-1]);
+  int burnin=0;  
+  vector<string> ale_files;
+  ale_files.push_back(first_file);
+  for (int i=2;i<argc;i++)
+    {
+      string next_field=argv[i];
+      vector <string> tokens;
+      boost::split(tokens,next_field,boost::is_any_of("="),boost::token_compress_on);
+      if (tokens[0]=="burnin")
+	burnin=atoi(tokens[1].c_str());
+      else
+	ale_files.push_back(argv[i]);
+    }
   ale=observe_ALE_from_file(ale_files,burnin);
-  cout << "# observe "<< ale->observations << "trees from: " <<  argv[1] ;
+  cout << "# observe "<< ale->observations << " tree(s) from: " <<  argv[1] ;
   for (int i=2;i<argc-1;i++)
     cout << " " << argv[i];
   cout << endl;
+  cout << burnin<<" burn in per file discarded."<<endl;
   ale->save_state(ale_name);
   cout << "# saved in "<< ale_name<<endl;
   return 1;
