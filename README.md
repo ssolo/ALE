a9b0f7deaeb379d2a78ad979d9e1a2e92b8a779d
Bastien Boussau <boussau@gmail.com>
2016-07-28 14:40:37 +0200

    Better README.md file and homogeneization of the command line interface between ALEml_undated and ALEmcmc_undated.

--- README.md
+++ README.md
@@ -1,18 +1,54 @@
 ALE
 ===
+ALE: Reconstruction of reconciled gene trees and estimation of DTL rates from a sample of gene trees and a rooted species tree.
 
-WARNING: We are currently transitioning to cmake and installation maybe buggy. If you encounter any problem please let us know.   
+
+WARNING: We have recently transitioned to cmake and installation may be buggy. If you encounter any problem please let us know.   
 
 Amalgamated likelihood estimation (ALE) is a probabilistic approach to exhaustively explore all reconciled gene trees that can be amalgamated as a combination of clades observed in a sample of gene trees. We implement the ALE approach in the context of a reconciliation model (cf. http://arxiv.org/abs/1211.4606 ), which allows for the duplication, transfer and loss of genes. We use ALE to efficiently approximate the sum of the joint likelihood over amalgamations and to find the reconciled gene tree that maximizes the joint likelihood among all such trees.   
 
-The repository currently impliemnets two objects:
+The repository currently implements two objects and several programs:
 
-1. The fundamental approx_posterior object that can be used to observe and store split frequencies and provides interfaces for performing basic Conditional Clade Probability (CCP, cf. Hohna and Drummond Systbiol. 2012) related, as well as more general Amalgamated Likelihood Estimation (ALE, cf. "Efficient Exploration of the Space of Reconciled Gene Trees", under review, soon available from the arxiv) related calculations.  
+1. The fundamental approx_posterior object that can be used to observe and store split frequencies and provides interfaces for performing basic Conditional Clade Probability (CCP, cf. Hohna and Drummond Systbiol. 2012) related, as well as caluclations related to more general Amalgamated Likelihood Estimation (ALE, cf. "Efficient Exploration of the Space of Reconciled Gene Trees", Syst Biol. 2013 Nov;62(6):901-12. doi: 10.1093/sysbio/syt054.).  
 
-2. The exODT_model object that impliments ALE in the context of our recently published reconcilation model (cf. http://arxiv.org/abs/1211.4606 ).
+2. The exODT_model object that implements ALE in the context of our recently published reconciliation model (cf. http://arxiv.org/abs/1211.4606 ).
 
+## Using ALE
+ALE reconciles a sample of gene trees with a species tree. The sample of gene trees is typically obtained from a Bayesian MCMC program (e.g. PhyloBayes or mrBayes or RevBayes...) or could be obtained using bootstrap replicates. The species tree needs to be rooted. Both inputs need to be provided as files containing Newick strings. ALE will use the sample of gene trees to piece together (amalgamate) reconciled gene trees while estimating their probabilities. Here the probability of a gene tree depends on both sequence information (provided by the gene tree frequencies in the initial sample) and rates of D, T, L events, which can be optimized or sampled depending on the program chosen. 2 models have been implemented for reconciling gene trees with a species tree. A dated model, which assumes that the nodes of the species tree are ordered relative to each other, and an undated model which only requires that the species tree is rooted.
 
-In lieu of more formal documentation please consult our manuscript "Efficient Exploration of the Space of Reconciled Gene Trees" (under review, soon available from the arxiv) together with the code and comments for the ALEobserve, ALEml, ALEsample programs that deploy these objects. 
+Two steps are required to obtained reconciled amalgamated gene trees.
+1. First, compact the sample of gene trees into an ALE object. This is done using the program ALEobserve.
+```sh
+ALEobserve geneFamily.treelist 1000  
+```
+Where geneFamily.treelist is the file containing the sample of gene trees, and 1000 is a number of gene trees that will be discarded. The result of the command above is to create a geneFamily.ale file that can be used to infer reconciled amalgamated gene trees during step 2.
+
+2. Second, compute the reconciled amalgamated gene trees. The DTL rates can be provided, or estimated. For the undated model, there are 2 ways to estimate these rates: Maximum Likelihood estimation with ALEml_undated, or Bayesian MCMC sampling with ALEmcmc_undated.
+
+### Maximum likelihood estimation with ALEml_undated
+DTL rates can either be estimated by providing no starting value or fixed to a starting value. Using those rates, the program then outputs a certain number of sampled reconciled amalgamated gene trees.
+```sh
+ALEml_undated species_tree.newick geneFamily.ale sample=number_of_samples separators=gene_name_separator O_R=OriginationAtRoot delta=DuplicationRate tau=TransferRate lambda=LossRate beta=weight_of_sequence_evidence
+```
+species_tree.newick : contains the Newick description of the rooted species tree.
+geneFamily.ale : contains the ALE built during step 1.
+sample=number_of_samples : how many reconciled gene trees should be output.
+separators=gene_name_separator : separator character used to separate species names from gene names (default to "_").
+O_R=OriginationAtRoot : rate of origination at root.
+delta=DuplicationRate : duplication rate.
+tau=TransferRate : transfer rate.
+lambda=LossRate : loss rate.
+beta=weight_of_sequence_evidence : how much sequence data will be trusted. Values higher than 1 mean the gene trees coming from the sequences alone is more trusted than by default; values lower than 1 mean that the gene trees seem not very trustworthy. Defaults to 1.
+
+### Bayesian MCMC sampling with ALEmcmc_undated
+DTL rates as well as the origination at the root rate are sampled. A prior value for these rates can be provided:
+```sh
+ALEmcmc_undated species_tree.newick gene_tree_sample.ale sample=number_of_samples separators=gene_name_separator O_R=OriginationAtRootPrior delta=DuplicationRatePrior tau=TransferRatePrior lambda=LossRatePrior sampling_rate=sampling_rate beta=weight_of_sequence_evidence
+```
+All the parameters correspond to those used by ALEml_undated, except for :
+sampling_rate=sampling_rate: during MCMC, sample a tree every sampling_rate iterations.
+
+For more documentation please consult our manuscript "Efficient Exploration of the Space of Reconciled Gene Trees" (Syst Biol. 2013 Nov;62(6):901-12. doi: 10.1093/sysbio/syt054.) together with the code and comments for the ALEobserve, ALEml_undated, ALEmcmc_undated, ALEml, ALEmcmc programs that deploy these objects.
 
 Example data and an associated README file are available in the example_data subdirectory.   
 
--- src/ALEml_undated.cpp
+++ src/ALEml_undated.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 using namespace bpp;
- 
+
 class p_fun:
   public virtual Function,
   public AbstractParametrizable
@@ -23,7 +23,7 @@ private:
   approx_posterior* ale_pointer;
 public:
   p_fun(exODT_model* model,approx_posterior* ale, double delta_start=0.01,double tau_start=0.01,double lambda_start=0.1//,double sigma_hat_start=1.
-	,bool delta_fixed_in=false,bool tau_fixed_in=false,bool lambda_fixed_in=false) : AbstractParametrizable(""), fval_(0), model_pointer(model), ale_pointer(ale) 
+	,bool delta_fixed_in=false,bool tau_fixed_in=false,bool lambda_fixed_in=false) : AbstractParametrizable(""), fval_(0), model_pointer(model), ale_pointer(ale)
   {
     //We declare parameters here:
  //   IncludingInterval* constraint = new IncludingInterval(1e-6, 10-1e-6);
@@ -31,14 +31,14 @@ public:
       delta_fixed=delta_fixed_in;
       tau_fixed=tau_fixed_in;
       lambda_fixed=lambda_fixed_in;
-      
+
       if (not delta_fixed)
 	{
 	  addParameter_( new Parameter("delta", delta_start, constraint) ) ;
 	  cout << "#optimizing delta rate"<< endl;
 	}
       if (not tau_fixed)
-	{	  
+	{
 	  addParameter_( new Parameter("tau", tau_start, constraint) ) ;
 	  cout << "#optimizing tau rate"<< endl;
 	}
@@ -49,11 +49,11 @@ public:
 	}
 
   }
-  
+
   p_fun* clone() const { return new p_fun(*this); }
-  
+
 public:
-  
+
     void setParameters(const ParameterList& pl)
     throw (ParameterNotFoundException, ConstraintException, Exception)
     {
@@ -65,17 +65,17 @@ public:
       if (not delta_fixed)
 	{
 	  double delta = getParameterValue("delta");
-	  model_pointer->set_model_parameter("delta",delta);	
+	  model_pointer->set_model_parameter("delta",delta);
 	}
       if (not tau_fixed)
 	{
 	  double tau = getParameterValue("tau");
-	  model_pointer->set_model_parameter("tau",tau);	
+	  model_pointer->set_model_parameter("tau",tau);
 	}
       if (not lambda_fixed)
 	{
 	  double lambda = getParameterValue("lambda");
-	  model_pointer->set_model_parameter("lambda",lambda);	
+	  model_pointer->set_model_parameter("lambda",lambda);
 	}
 
       model_pointer->calculate_undatedEs();
@@ -91,9 +91,12 @@ int main(int argc, char ** argv)
 {
   cout << "ALEml using ALE v"<< ALE_VERSION <<endl;
 
-  if (argc<3) 
+  if (argc<3)
     {
-      cout << "usage:\n ./ALEml_undated species_tree.newick gene_tree_sample.ale [samples] [gene_name_separator] [OriginationAtRoot] [DuplicationRate] [TransferRate] [LossRate]" << endl;
+      cout << "\nUsage:\n ./ALEml_undated species_tree.newick gene_tree_sample.ale sample=number_of_samples separators=gene_name_separator O_R=OriginationAtRoot delta=DuplicationRate tau=TransferRate lambda=LossRate beta=weight_of_sequence_evidence" << endl;
+      cout << "\n1st example: we arbitrarily set the DTL values and do not perform any optimization \n ./ALEml_undated species_tree.newick gene_tree_sample.ale sample=100 separators=_ delta=0.05 tau=0.1 lambda=0.2 " << endl;
+      cout << "\n2nd example: we arbitrarily set the T value to 0 to get a DL-only model and optimize the DL parameters \n ./ALEml_undated species_tree.newick gene_tree_sample.ale sample=100 separators=_ tau=0\n" << endl;
+
       return 1;
     }
 
@@ -114,56 +117,56 @@ int main(int argc, char ** argv)
   exODT_model* model=new exODT_model();
 
   scalar_type samples=100;
-  scalar_type O_R=1,beta=1; 
+  scalar_type O_R=1,beta=1;
   bool delta_fixed=false;
   bool tau_fixed=false;
   bool lambda_fixed=false;
-  scalar_type delta=0.01,tau=0.01,lambda=0.1;  
+  scalar_type delta=0.01,tau=0.01,lambda=0.1;
 
   for (int i=3;i<argc;i++)
+  {
+    string next_field=argv[i];
+    vector <string> tokens;
+    boost::split(tokens,next_field,boost::is_any_of("="),boost::token_compress_on);
+    if (tokens[0]=="sample")
+    samples=atoi(tokens[1].c_str());
+    else if (tokens[0]=="separators")
+    model->set_model_parameter("gene_name_separators", tokens[1]);
+    else if (tokens[0]=="delta")
     {
-      string next_field=argv[i];
-      vector <string> tokens;
-      boost::split(tokens,next_field,boost::is_any_of("="),boost::token_compress_on);
-      if (tokens[0]=="sample")
-	samples=atoi(tokens[1].c_str());
-      else if (tokens[0]=="separators")
-	model->set_model_parameter("gene_name_separators", tokens[1]);
-      else if (tokens[0]=="delta")
-	{
-	  delta=atof(tokens[1].c_str());
-	  delta_fixed=true;
-	  cout << "# delta fixed to " << delta << endl; 
-	}
-      else if (tokens[0]=="tau")
-	{
-	  tau=atof(tokens[1].c_str());
-	  tau_fixed=true;
-	  cout << "# tau fixed to " << tau << endl; 
-	}
-      else if (tokens[0]=="lambda")
-       	{
-	  lambda=atof(tokens[1].c_str());
-	  lambda_fixed=true;
-	  cout << "# lambda fixed to " << lambda << endl; 
-
-	}
-      else if (tokens[0]=="O_R")
-	{
-	  O_R=atof(tokens[1].c_str());
-	  cout << "# O_R set to " << O_R << endl; 
-	}
-      else if (tokens[0]=="beta")
-	{
-	  beta=atof(tokens[1].c_str());
-	  cout << "# beta set to " << beta << endl; 
-
-	}
+      delta=atof(tokens[1].c_str());
+      delta_fixed=true;
+      cout << "# delta fixed to " << delta << endl;
     }
+    else if (tokens[0]=="tau")
+    {
+      tau=atof(tokens[1].c_str());
+      tau_fixed=true;
+      cout << "# tau fixed to " << tau << endl;
+    }
+    else if (tokens[0]=="lambda")
+    {
+      lambda=atof(tokens[1].c_str());
+      lambda_fixed=true;
+      cout << "# lambda fixed to " << lambda << endl;
 
-  model->set_model_parameter("BOOT_STRAP_LABLES","yes");  
+    }
+    else if (tokens[0]=="O_R")
+    {
+      O_R=atof(tokens[1].c_str());
+      cout << "# O_R set to " << O_R << endl;
+    }
+    else if (tokens[0]=="beta")
+    {
+      beta=atof(tokens[1].c_str());
+      cout << "# beta set to " << beta << endl;
+
+    }
+  }
+
+  model->set_model_parameter("BOOT_STRAP_LABLES","yes");
   model->construct_undated(Sstring);
-  
+
   model->set_model_parameter("seq_beta", beta);
   model->set_model_parameter("O_R", O_R);
   //a set of inital rates
@@ -183,10 +186,10 @@ int main(int argc, char ** argv)
   optimizer->setProfiler(0);
   optimizer->setMessageHandler(0);
   optimizer->setVerbose(2);
-  
+
   optimizer->setConstraintPolicy(AutoParameter::CONSTRAINTS_AUTO);
   optimizer->init(f->getParameters()); //Here we optimize all parameters, and start with the default values.
-        
+
   scalar_type mlll;
   if (not (delta_fixed and tau_fixed and lambda_fixed) )
     {
@@ -203,23 +206,23 @@ int main(int argc, char ** argv)
     }
   cout << endl << "ML rates: " << " delta=" << delta << "; tau=" << tau << "; lambda="<<lambda//<<"; sigma="<<sigma_hat
        <<"."<<endl;
-  
-  
+
+
   cout << "LL=" << mlll << endl;
-  
+
   cout << "Sampling reconciled gene trees.."<<endl;
   vector <string> sample_strings;
   vector <Tree*> sample_trees;
   boost::progress_display pd( samples );
 
-  for (int i=0;i<samples;i++) 
+  for (int i=0;i<samples;i++)
     {
       ++pd;
       string sample_tree=model->sample_undated();
-      sample_strings.push_back(sample_tree);      
+      sample_strings.push_back(sample_tree);
       if (ale->last_leafset_id>3)
 	{
-	  
+
 	  tree_type * G=TreeTemplateTools::parenthesisToTree(sample_tree,false);
 
 	  vector<Node*> leaves = G->getLeaves();
@@ -235,14 +238,14 @@ int main(int argc, char ** argv)
 	  sample_trees.push_back(G);
 	}
     }
-  /*cout << "Calculating ML reconciled gene tree.."<<endl; 
-  pair<string, scalar_type> res = model->p_MLRec(ale);    
+  /*cout << "Calculating ML reconciled gene tree.."<<endl;
+  pair<string, scalar_type> res = model->p_MLRec(ale);
   //and output it..
   */
   vector<string> tokens;
   boost::split(tokens,ale_file,boost::is_any_of("/"),boost::token_compress_on);
   ale_file=tokens[tokens.size()-1];
-  string outname=ale_file+".uml_rec"; 
+  string outname=ale_file+".uml_rec";
   ofstream fout( outname.c_str() );
   fout <<  "#ALEml_undated using ALE v"<< ALE_VERSION <<" by Szollosi GJ et al.; ssolo@elte.hu; CC BY-SA 3.0;"<<endl<<endl;
   fout << "S:\t"<<model->string_parameter["S_with_ranks"] <<endl;
@@ -258,22 +261,22 @@ int main(int argc, char ** argv)
     {
       fout<<sample_strings[i]<<endl;
     }
-  
+
   //fout << "reconciled G:\t"<< res.first <<endl;
-  fout << "# of\t Duplications\tTransfers\tLosses\tSpeciations" <<endl; 
-  fout <<"Total \t"<< model->MLRec_events["D"]/samples << "\t" << model->MLRec_events["T"]/samples << "\t" << model->MLRec_events["L"]/samples<< "\t" << model->MLRec_events["S"]/samples <<endl;    
+  fout << "# of\t Duplications\tTransfers\tLosses\tSpeciations" <<endl;
+  fout <<"Total \t"<< model->MLRec_events["D"]/samples << "\t" << model->MLRec_events["T"]/samples << "\t" << model->MLRec_events["L"]/samples<< "\t" << model->MLRec_events["S"]/samples <<endl;
   fout << endl;
-  fout << "# of\t Duplications\tTransfers\tLosses\tOriginations\tcopies" <<endl; 
+  fout << "# of\t Duplications\tTransfers\tLosses\tOriginations\tcopies" <<endl;
   fout << model->counts_string_undated(samples);
-  
+
   cout << "Results in: " << outname << endl;
   if (ale->last_leafset_id>3)
     {
       cout << "Calculating consensus tree."<<endl;
       Tree* con_tree= TreeTools::thresholdConsensus(sample_trees,0.5);
-      
+
       string con_name=ale_file+".ucons_tree";
-      
+
       ofstream con_out( con_name.c_str() );
       con_out <<  "#ALEsample using ALE v"<< ALE_VERSION <<" by Szollosi GJ et al.; ssolo@elte.hu; CC BY-SA 3.0;"<<endl;
       TreeTools::computeBootstrapValues(*con_tree,sample_trees);
@@ -281,11 +284,11 @@ int main(int argc, char ** argv)
       con_out << con_tree_sup << endl;
       cout << endl<< "Consensus tree in " << con_name<< endl;
     }
-  
+
   string t_name=ale_file+".uTs";
   ofstream tout( t_name.c_str() );
   tout <<"#from\tto\tfreq.\n";
-  
+
   for (int e=0;e<model->last_branch;e++)
     for (int f=0;f<model->last_branch;f++)
       if  (model->T_to_from[e][f]>0)
@@ -295,7 +298,7 @@ int main(int argc, char ** argv)
 	  else
 	    tout << "\t" << e;
 	  if (f<model->last_leaf)
-	    tout << "\t" << model->node_name[model->id_nodes[f]];	      
+	    tout << "\t" << model->node_name[model->id_nodes[f]];
 	  else
 	    tout << "\t" << f;
 	  tout << "\t" << model->T_to_from[e][f]/samples <<  endl;
@@ -303,4 +306,3 @@ int main(int argc, char ** argv)
   cout << "Transfers in: " << t_name << endl;
   return 0;
 }
-
