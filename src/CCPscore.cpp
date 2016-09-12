87820a18924f10c4f5970a9e7acb966e7113be8f
ssolo <sszolo@gmail.com>
2016-09-12 12:28:50 +0200

    CCPscore, and simplified CMakeLists.txt

---
+++ src/CCPscore.cpp
@@ -0,0 +1,19 @@
+
+#include "exODT.h"
+#include "ALE_util.h"
+
+using namespace std;
+using namespace bpp;
+int main(int argc, char ** argv)
+{
+  string ale_file=argv[1];
+  approx_posterior * ale;
+
+  ale=load_ALE_from_file(ale_file);
+  ifstream tree_stream (argv[2]);
+  string tree;
+  getline (tree_stream,tree);
+  cout << log(ale->p(tree)) << endl;
+
+  return 1;
+}
