91809c1abfd9a37bea1ddca84f08f17fe2ccf058
Gergely J Szöllősi <sszolo@gmail.com>
2018-12-05 20:18:11 +0100

    new version with rate_multipliers, some output format changes, and more precise numerical ML

--- src/ALE.h
+++ src/ALE.h
@@ -1,7 +1,7 @@
 //all code by Szollosi GJ et al.; ssolo@elte.hu; GNU GPL 3.0;
 #pragma once
 
-#define ALE_VERSION "0.4"
+#define ALE_VERSION "0.5"
 
 #include <iostream>
 #include <Bpp/Phyl/BipartitionTools.h>
