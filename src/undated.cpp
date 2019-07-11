cd1ca65a2b11973efc3d6b822a16928a895c084d
Gergely J Szöllősi <sszolo@gmail.com>
2019-07-11 10:52:59 +0200

    minor bug

--- src/undated.cpp
+++ src/undated.cpp
@@ -1052,7 +1052,7 @@ string exODT_model::counts_string_undated(scalar_type samples)
     << branch_counts["Ts"][e]/samples << "\t"
     << branch_counts["Ls"][e]/samples << "\t"
     << branch_counts["Os"][e]/samples << "\t"
-    << branch_counts["singleton"][e] << "\t"
+    << branch_counts["singleton"][e]/samples  << "\t"
     << branch_counts["copies"][e]/samples << "\t"
     << uE[e] << "\n";
 
